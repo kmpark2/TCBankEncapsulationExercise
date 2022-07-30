13f19fce031dc5f685e3b091b7181e1212dcebfd
kmpark2 <98770234+kmpark2@users.noreply.github.com>
2022-07-30 20:54:14 +0900

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double deposit)
+        {
+            balance += deposit;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much will you deposit today? (in dollars):");
+            double userDeposit = double.Parse(Console.ReadLine());
+            BankAccount userAccount = new BankAccount();
+            userAccount.Deposit(userDeposit);
+            Console.WriteLine($"Your account's balance is {userAccount.GetBalance()} dollars.");
+        }
+    }
+}
